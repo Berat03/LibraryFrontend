8dd6e7b873019f25aad8209196eeead4a81c4d19
berat03 <bbulbulwork@gmail.com>
2024-02-12 01:35:17 +0000

    SPA-esq homepage line and pie chart

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,31 @@
+// HomePage.js
 import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import LandingPie from "../components/LandingPie";
+import LineChartFixedHours from "../components/LineChartFixedHours";
+import CathedralImage from "../images/durhamNotMyImageRECropped.png";
 
 export default function HomePage() {
-    const [col, setCol] = useState("bg-yellow-300");
-    // style={{backgroundImage: "url('/durhamNotMyimage.jpeg')"}
-    return (
-        <div className={`${col} h-screen`}>
-            <div id="navbar" className={'font-bold text-white'}>
-                <NavBar col={col} setCol={setCol} />
-            </div>
+    const [showPieChart, setShowPieChart] = useState(true);
 
-            <div id="center" className="flex justify-center">
-                <LandingPie />
+    const handleChartToggle = () => {
+        setShowPieChart(!showPieChart);
+    };
+
+    const backgroundImageStyle = {
+        backgroundImage: `url(${CathedralImage})`
+    };
+
+    return (
+        <div style={backgroundImageStyle} className="min-h-screen bg-cover bg-no-repeat bg-center flex flex-col justify-center">
+            <div className="text-center p-1 font-bold text-white w-full">
+                <NavBar />
+            </div>
+            <div className="flex-grow flex justify-center items-center">
+                {showPieChart
+                    ? <LandingPie key="pieChart" onButtonClick={handleChartToggle} />
+                    : <LineChartFixedHours key="barChart" onButtonClick={handleChartToggle} />}
             </div>
         </div>
     );
-};
+}
