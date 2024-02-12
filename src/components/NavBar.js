3920e68524462bac613d1095a710d0e23b6d6006
berat03 <bbulbulwork@gmail.com>
2024-02-12 01:33:23 +0000

    un-generalised navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,26 @@
 import React from "react";
-import DataContactBar from "./DataContactBar";
-import Title from "./Title";
+import TitleDate from "./TitleDate";
+import {Link} from "react-router-dom";
 
-
-export default function NavBar(props) {
-
-
+export default function NavBar() {
     return (
-        <div className="flex h-20 items-center justify-between">
-            <div className="flex-1"></div> {/* Invisible spacer */}
-            <div className="flex-1 justify-center flex">
-                <Title />
+        <div className="flex justify-between items-center text-2xl text-white w-full">
+            <div className="flex">
+                <div>
+                    <TitleDate />
+                </div>
             </div>
-            <div className="flex-1">
-                <DataContactBar col={props.col} setCol={props.setCol} />
+
+            {/* This div acts as a spacer, pushing the Title and PageNavBar to opposite ends */}
+            <div className="flex">
+                <h1 className="text-4xl"> BILLY B OCCUPANCY</h1>
+            </div>
+
+            <div className="flex">
+                <Link className="mr-7" to={'/'}> HOME </Link>
+                <Link className="mr-6" to={'/data'}> DATA </Link>
+                <Link to={'/contact'}> CONTACT</Link>
             </div>
         </div>
-    )
+    );
 }
