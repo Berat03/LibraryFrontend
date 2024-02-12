bfa97f7982b3546199ad68f14a64becbc5fe5dda
berat03 <bbulbulwork@gmail.com>
2024-02-12 16:26:46 +0000

    added hover to button, needed to use inline css

--- src/components/ChangeGraphButton.js
+++ src/components/ChangeGraphButton.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ChangeGraphButton({ onClick, text }) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const buttonStyle = {
+        backgroundColor: isHovered ? 'rgb(17 24 39)' : 'rgb(146,53,180)', // Use gray for hover, your color otherwise
+        transition: 'background-color 150ms ease-in-out', // Smooth transition for background color
+    };
+
     return (
         <button
             type="button"
             onClick={onClick}
-            className="rounded-full px-4 py-2.5 text-sm font-semibold text-white shadow-sm"
-            style ={{backgroundColor: 'rgb(146,53,180)'}}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className="rounded-md px-4 py-2.5 text-base font-semibold text-white shadow-sm"
+            style={buttonStyle}
         >
             {text}
         </button>
