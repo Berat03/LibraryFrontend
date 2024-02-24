b4f0aee39daab088e86465e65463b5f8fe22e22f
berat03 <bbulbulwork@gmail.com>
2024-02-24 19:49:08 +0000

    these APIs are throttled and tracked!

---
+++ src/API/apiEndpoint.js
@@ -0,0 +1,3 @@
+export const apiEndpoint = 'https://xfafqazw4m.execute-api.eu-west-2.amazonaws.com/Test/queryData';
+// API key is throttled (completed) and CORS confined (not yet)
+export const apiQuery = 'https://a37zsv71n3.execute-api.eu-west-2.amazonaws.com/default/forecastOccupancy';
